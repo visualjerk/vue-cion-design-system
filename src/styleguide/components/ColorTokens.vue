84670ef5e913ba58f0ec67f3754b25eb70376577
JB <jb@visualjerk.de>
2018-08-10 23:33:03 +0200

    color tokens

--- src/styleguide/components/ColorTokens.vue
+++ src/styleguide/components/ColorTokens.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
     <h2>Color Tokens</h2>
+    <div
+      v-for="(group, key) in colorGroups"
+      :key="key">
+      <h3>{{ group.description }}</h3>
+      <div 
+        v-for="color in tokenMap[key]" 
+        :key="color.name">
+        <div :style="{ backgroundColor: color.value, height: '30px' }" />
+        {{ color.scss }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ColorTokens'
+  name: 'ColorTokens',
+  data() {
+    return {
+      colorGroups: {
+        colorGroup1: {
+          description: 'First colors'
+        },
+        colorGroup2: {
+          description: 'Second colors'
+        },
+        colorGroup3: {
+          description: 'Third colors'
+        },
+        colorGroup4: {
+          description: '4th colors'
+        },
+        colorGroup5: {
+          description: '5th colors'
+        }
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-h2 {
-  color: $color-blue-bis;
-}
 </style>
