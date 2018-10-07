5f8dbc62f549f555aa463c1e6fc8ec340a81271e
JB <jb@visualjerk.de>
2018-10-07 22:59:36 +0200

    fix copy to clipboard for ios

--- dist/system.common.js
+++ dist/system.common.js
@@ -12050,6 +12050,71 @@ module.exports = { render: render };
 
 /***/ }),
 
+/***/ "f1cd":
+/***/ (function(module, exports) {
+
+module.exports = clipboardCopy
+
+function clipboardCopy (text) {
+  // Use the Async Clipboard API when available
+  if (navigator.clipboard) {
+    return navigator.clipboard.writeText(text)
+  }
+
+  // ...Otherwise, use document.execCommand() fallback
+
+  // Put the text to copy into a <span>
+  var span = document.createElement('span')
+  span.textContent = text
+
+  // Preserve consecutive spaces and newlines
+  span.style.whiteSpace = 'pre'
+
+  // An <iframe> isolates the <span> from the page's styles
+  var iframe = document.createElement('iframe')
+  iframe.sandbox = 'allow-same-origin'
+
+  // Add the <iframe> to the page
+  document.body.appendChild(iframe)
+  var win = iframe.contentWindow
+
+  // Add the <span> to the <iframe>
+  win.document.body.appendChild(span)
+
+  // Get a Selection object representing the range of text selected by the user
+  var selection = win.getSelection()
+
+  // Fallback for Firefox which fails to get a selection from an <iframe>
+  if (!selection) {
+    win = window
+    selection = win.getSelection()
+    document.body.appendChild(span)
+  }
+
+  var range = win.document.createRange()
+  selection.removeAllRanges()
+  range.selectNode(span)
+  selection.addRange(range)
+
+  var success = false
+  try {
+    success = win.document.execCommand('copy')
+  } catch (err) {}
+
+  selection.removeAllRanges()
+  win.document.body.removeChild(span)
+  document.body.removeChild(iframe)
+
+  // The Async Clipboard API returns a promise that may reject with `undefined` so we
+  // match that here for consistency.
+  return success
+    ? Promise.resolve()
+    : Promise.reject() // eslint-disable-line prefer-promise-reject-errors
+}
+
+
+/***/ }),
+
 /***/ "f22a":
 /***/ (function(module, exports) {
 
@@ -12597,27 +12662,18 @@ var kebabCase_default = /*#__PURE__*/__webpack_require__.n(kebabCase);
     Vue.filter('kebabCase', kebabCase_default.a);
   }
 });
+// EXTERNAL MODULE: ./node_modules/clipboard-copy/index.js
+var clipboard_copy = __webpack_require__("f1cd");
+var clipboard_copy_default = /*#__PURE__*/__webpack_require__.n(clipboard_copy);
+
 // CONCATENATED MODULE: ./src/system/plugins/utils.js
+
 /* harmony default export */ var utils = ({
   install: function install(Vue) {
     Vue.mixin({
       methods: {
         $copyToClipboard: function $copyToClipboard(content) {
-          var el = document.createElement('textarea');
-          el.value = content;
-          el.setAttribute('readonly', '');
-          el.style.position = 'absolute';
-          el.style.left = '-9999px';
-          document.body.appendChild(el);
-          var selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
-          el.select();
-          document.execCommand('copy');
-          document.body.removeChild(el);
-
-          if (selected) {
-            document.getSelection().removeAllRanges();
-            document.getSelection().addRange(selected);
-          }
+          return clipboard_copy_default()(content);
         }
       }
     });
