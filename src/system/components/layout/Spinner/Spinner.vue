0a29c178eaea59484e81abc6b9902d5f5b0a4b6e
Grzegorz Leoniec <greg@app-interactive.de>
2019-01-13 20:55:08 +0100

    Added spinner

---
+++ src/system/components/layout/Spinner/Spinner.vue
@@ -0,0 +1,43 @@
+<template>
+  <svg 
+    viewBox="0 0 50 50"
+    class="ds-spinner"
+    :class="classes"
+  >
+    <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'DsSpinner',
+  props: {
+    size: {
+      type: String,
+      default: 'base',
+      validator: value => {
+        return value.match(/(small|base|large)/)
+      }
+    },
+    inverse: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classes() {
+      let classes = [`ds-size-${this.size}`]
+      if (this.inverse) {
+        classes.push('ds-spinner-inverse')
+      }
+
+      return classes
+    }
+  }
+}
+</script>
+
+<docs src="./demo.md"></docs>
+
+<style lang="scss" src="./style.scss">
+</style>
